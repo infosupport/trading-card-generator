7674d1a29cb80811276c566b172482d6be1477be
Willem Meints <1550763+wmeints@users.noreply.github.com>
2025-07-30 16:35:53 +0200

    Add Semantic Kernel components

--- src/InfoSupport.TradingCardGenerator/Program.cs
+++ src/InfoSupport.TradingCardGenerator/Program.cs
@@ -1,6 +1,14 @@
 using InfoSupport.TradingCardGenerator.Endpoints;
+using Microsoft.SemanticKernel;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddKernel()
+    .AddAzureOpenAITextToImage(
+        builder.Configuration["LanguageModel:ImageModel"]!,
+        builder.Configuration["LanguageModel:Endpoint"]!,
+        builder.Configuration["LanguageModel:ApiKey"]!
+    );
 
 var app = builder.Build();
 
